a574a59c173dde689ff24152ff7feecc29feabc8
Sanyam Garg <shanujindal48@gmail.com>
2023-02-24 08:36:43 -0500

    add download functionality to gateway

--- gateway/server.py
+++ gateway/server.py
@@ -1,17 +1,19 @@
 # gridfs helps to handle files larger than 16MB
 import os, gridfs, pika, json
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_pymongo import PyMongo
 from auth import validate
 from auth_svc import access
 from storage import util
+from bson.objectid import ObjectID
 
 server = Flask(__name__)
-server.config["MONGO_URI"] = "mongodb://host.internal.minikube:27017/videos"
 
-mongo = PyMongo(server)
+mongo_video = PyMongo(server, uri="mongodb://host.internal.minikube:27017/videos")
+mongo_mp3 = PyMongo(server, uri="mongodb://host.internal.minikube:27017/mp3s")
 
-fs = gridfs.GridFS(mongo.db)
+fs_videos = gridfs.GridFS(mongo_video.db)
+fs_mp3s = gridfs.GridFS(mongo_mp3.db)
 
 # "rabbitmq" refers to the rabbit MQ host in the cluster. Will be configured later.
 connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
@@ -37,7 +39,7 @@ def upload():
                 return "exactly 1 file required", 400
             
             for _, f in request.files.items():
-                err = util.upload(f, fs, channel, access)
+                err = util.upload(f, fs_videos, channel, access)
                 if err:
                     return err
             
@@ -47,7 +49,24 @@ def upload():
 
 @server.route("/download", methods=["GET"])
 def download():
-    pass
+    access, err = validate.token(request)
+    
+    if not err:
+        access = json.loads(access)
+        if access["admin"]:
+            fid_string = request.args.get("fid")
+            
+            if not fid_string:
+                return "fid is required", 400
+            
+            try:
+                out = fs_mp3s.get(ObjectID(fid_string))
+                return send_file(out, download_name=f"{fid_string}.mp3")
+            except Exception as err:
+                print(err)
+                return "internal server error", 500
+        
+        return "not authorized", 401
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=8080)
